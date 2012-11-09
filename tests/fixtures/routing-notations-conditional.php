1a9ec95b7dfa3462ff77281088dcdd6650978088
Mārtiņš Tereško <martins.teresko@live.com>
2012-11-09 17:09:44 +0200

    added new possible notation form

--- tests/fixtures/routing-notations-conditional.php
+++ tests/fixtures/routing-notations-conditional.php
@@ -15,7 +15,12 @@
              [ 'notation'   => '/note/:note/:notepad/',
                'conditions' => [ 'note'    => '00[a-z]{2,4}',
                                  'notepad' => '[qwerty]?' ],
-               'pattern'    => '#/note/(?P<note>00[a-z]{2,4})/(?P<notepad>[qwerty]?)#' ] ];
+               'pattern'    => '#/note/(?P<note>00[a-z]{2,4})/(?P<notepad>[qwerty]?)#' ], 
+
+             [ 'notation'   => '/i[[/:module]/:param][-:id]',
+               'conditions' => [ 'param' => '[a-z]+',
+                                 'id'    => '[\d]{1,2}' ],
+               'pattern'    => '#/i(:?(:?/(?P<module>[^/\.,;?\n]+))?/(?P<param>[a-z]+))?(:?-(?P<id>[\d]{1,2}))?#' ] ];
 
 /*
              [ 'notation'   => '',
