2bcf0f645236067d82dc0b19275d840f9ad83241
Mārtiņš Tereško <martins.teresko@live.com>
2012-11-28 19:41:35 +0200

    changed test patterns for expected routing expressions

--- tests/fixtures/routing-notations-conditional.php
+++ tests/fixtures/routing-notations-conditional.php
@@ -2,25 +2,25 @@
 
     return [ [ 'notation'   => '',
                'conditions' => [],
-               'pattern'    => '##' ],
+               'pattern'    => '#^$#' ],
 
              [ 'notation'   => ':standard',
                'conditions' => [],
-               'pattern'    => '#/(?P<standard>[^/\.,;?\n]+)#' ],
+               'pattern'    => '#^/(?P<standard>[^/\.,;?\n]+)$#' ],
 
              [ 'notation'   => ':simple',
                'conditions' => [ 'simple'  => '[0-9]+'],
-               'pattern'    => '#/(?P<simple>[0-9]+)#' ],
+               'pattern'    => '#^/(?P<simple>[0-9]+)$#' ],
 
              [ 'notation'   => '/note/:note/:notepad/',
                'conditions' => [ 'note'    => '00[a-z]{2,4}',
                                  'notepad' => '[qwerty]?' ],
-               'pattern'    => '#/note/(?P<note>00[a-z]{2,4})/(?P<notepad>[qwerty]?)#' ], 
+               'pattern'    => '#^/note/(?P<note>00[a-z]{2,4})/(?P<notepad>[qwerty]?)$#' ], 
 
              [ 'notation'   => '/i[[/:module]/:param][-:id]',
                'conditions' => [ 'param' => '[a-z]+',
                                  'id'    => '[\d]{1,2}' ],
-               'pattern'    => '#/i(:?(:?/(?P<module>[^/\.,;?\n]+))?/(?P<param>[a-z]+))?(:?-(?P<id>[\d]{1,2}))?#' ] ];
+               'pattern'    => '#^/i(:?(:?/(?P<module>[^/\.,;?\n]+))?/(?P<param>[a-z]+))?(:?-(?P<id>[\d]{1,2}))?$#' ] ];
 
 /*
              [ 'notation'   => '',
--- tests/fixtures/routing-notations-simple.php
+++ tests/fixtures/routing-notations-simple.php
@@ -2,43 +2,43 @@
 
     
     return [ [ 'notation' => '/test',
-               'pattern'  => '#/test#' ],
+               'pattern'  => '#^/test$#' ],
 
              [ 'notation' => 'foo/bar',
-               'pattern'  => '#/foo/bar#' ],
+               'pattern'  => '#^/foo/bar$#' ],
 
              [ 'notation' => '/:page',
-               'pattern'  => '#/(?P<page>[^/\.,;?\n]+)#' ],
+               'pattern'  => '#^/(?P<page>[^/\.,;?\n]+)$#' ],
 
              [ 'notation' => '/:resource/view',
-               'pattern'  => '#/(?P<resource>[^/\.,;?\n]+)/view#' ],
+               'pattern'  => '#^/(?P<resource>[^/\.,;?\n]+)/view$#' ],
 
              [ 'notation' => 'user/:id/:nickname',
-               'pattern'  => '#/user/(?P<id>[^/\.,;?\n]+)/(?P<nickname>[^/\.,;?\n]+)#' ],
+               'pattern'  => '#^/user/(?P<id>[^/\.,;?\n]+)/(?P<nickname>[^/\.,;?\n]+)$#' ],
 
              [ 'notation' => '',
-               'pattern'  => '##' ],
+               'pattern'  => '#^$#' ],
 
              [ 'notation' => '[lorem/ipsum]',
-               'pattern'  => '#(:?/lorem/ipsum)?#' ],
+               'pattern'  => '#^(:?/lorem/ipsum)?$#' ],
 
              [ 'notation' => '[/:optional]',
-               'pattern'  => '#(:?/(?P<optional>[^/\.,;?\n]+))?#' ],
+               'pattern'  => '#^(:?/(?P<optional>[^/\.,;?\n]+))?$#' ],
 
              [ 'notation' => 'test[/:id]',
-               'pattern'  => '#/test(:?/(?P<id>[^/\.,;?\n]+))?#' ],
+               'pattern'  => '#^/test(:?/(?P<id>[^/\.,;?\n]+))?$#' ],
 
              [ 'notation' => '[/:one][/:two]',
-               'pattern'  => '#(:?/(?P<one>[^/\.,;?\n]+))?(:?/(?P<two>[^/\.,;?\n]+))?#' ],
+               'pattern'  => '#^(:?/(?P<one>[^/\.,;?\n]+))?(:?/(?P<two>[^/\.,;?\n]+))?$#' ],
 
              [ 'notation' => '[/test/:parameter]/mandatory',
-               'pattern'  => '#(:?/test/(?P<parameter>[^/\.,;?\n]+))?/mandatory#' ],
+               'pattern'  => '#^(:?/test/(?P<parameter>[^/\.,;?\n]+))?/mandatory$#' ],
 
              [ 'notation' => '[/:controller[/:action][/:key]]',
-               'pattern'  => '#(:?/(?P<controller>[^/\.,;?\n]+)(:?/(?P<action>[^/\.,;?\n]+))?(:?/(?P<key>[^/\.,;?\n]+))?)?#' ],
+               'pattern'  => '#^(:?/(?P<controller>[^/\.,;?\n]+)(:?/(?P<action>[^/\.,;?\n]+))?(:?/(?P<key>[^/\.,;?\n]+))?)?$#' ],
 
              [ 'notation' => '[[/:minor]/:major]',
-               'pattern'  => '#(:?(:?/(?P<minor>[^/\.,;?\n]+))?/(?P<major>[^/\.,;?\n]+))?#' ] ];
+               'pattern'  => '#^(:?(:?/(?P<minor>[^/\.,;?\n]+))?/(?P<major>[^/\.,;?\n]+))?$#' ] ];
 
 /*
              [ 'notation' => '',
