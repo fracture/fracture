202f66ff3969edde29e61ce36cc74139321f9332
Mārtiņš Tereško <martins.teresko@live.com>
2012-12-08 16:26:43 +0200

    innitial tests for Router class

---
+++ tests/lib/fracture/routing/routerTest.php
@@ -0,0 +1,82 @@
+<?php
+
+
+    namespace Fracture\Routing;
+
+    use Exception;
+    use ReflectionClass;
+    use PHPUnit_Framework_TestCase;
+
+
+    class RouterTest extends PHPUnit_Framework_TestCase
+    {
+
+        /**
+         * @covers Router::import
+         */
+        public function test_Calling_Create_in_Import_for_Single_Route()
+        {
+            $builder = $this->getMock( 'RouteBuilder', ['create'] );
+            $builder->expects($this->once())
+                    ->method('create')
+                    ->with($this->equalTo('test'),
+                           $this->equalTo( [ 'notation' => '[/:alpha][:/beta]',
+                                             'defaults' => [
+                                                "alpha" => 'qux',
+                                                "beta"  => 'qux' ]]));
+
+            $router = new Router( $builder );
+            $router->import( __DIR__ . '/../../../fixtures/configs/routes-single.json' );
+            
+        }
+
+
+        /**
+         * @covers Router::import
+         */
+        public function test_Calling_Create_in_Import_for_Several_Routes()
+        {
+            $builder = $this->getMock( 'RouteBuilder', ['create'] );
+            $builder->expects($this->exactly(3))
+                     ->method('create');
+
+            $router = new Router( $builder );
+            $router->import( __DIR__ . '/../../../fixtures/configs/routes-multiple.json' );
+            
+        }
+
+
+        /**
+         * @covers Router::import
+         */
+        public function test_Invalid_JSON_Import_Exception()
+        {
+
+            $this->setExpectedException('Exception');
+            $builder = $this->getMock( 'RouteBuilder', ['create'] );
+
+            $router = new Router( $builder );
+            $router->import( __DIR__ . '/../../../fixtures/configs/routes-invalid.json' );
+
+        }
+
+
+        /**
+         * @covers Router::import
+         */
+        public function test_Missing_JSON_Exception()
+        {
+
+            $this->setExpectedException('Exception');
+            $builder = $this->getMock( 'RouteBuilder', ['create'] );
+
+            $router = new Router( $builder );
+            $router->import( __DIR__ . '/../../../fixtures/configs/routes-fake.json' );
+
+        }
+
+
+    }
+
+
+
