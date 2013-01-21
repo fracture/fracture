49744427644c851f537ab9d7733b1bb285d83346
Mārtiņš Tereško <martins.teresko@live.com>
2013-01-21 03:53:50 +0200

    test case for the builder to see if it returns expected instance

---
+++ tests/lib/fracture/routing/requestbuilderTest.php
@@ -0,0 +1,24 @@
+<?php
+
+
+    namespace Fracture\Routing;
+
+    use Exception;
+    use ReflectionClass;
+    use PHPUnit_Framework_TestCase;
+
+
+    class requestBuilderTest extends PHPUnit_Framework_TestCase
+    {
+
+        /**
+         * @covers \Fracture\Routing\RequestBuilder::build()
+         */
+        public function testCreate()
+        {
+            $builder = new RequestBuilder;
+            $this->assertInstanceOf('\Fracture\Routing\UserRequest', $builder->create());
+        }
+
+
+    }
