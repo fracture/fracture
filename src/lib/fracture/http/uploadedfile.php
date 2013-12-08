62bf356caaf5fff76d160d02834a73c84dc59a65
Mārtiņš Tereško <martins.teresko@live.com>
2013-12-08 16:52:02 +0200

    adding wrapper for $_FILES entries

---
+++ src/lib/fracture/http/uploadedfile.php
@@ -0,0 +1,46 @@
+<?php
+	
+	namespace Fracture\Http;
+
+	class UploadedFile
+	{
+
+		private $rawParams = [];
+
+
+		public function __construct( $params )
+		{
+			$this->rawParams = $params;
+		}
+
+
+		public function getName()
+		{
+			return $this->rawParams['name'];
+		}
+
+
+		public function getSize()
+		{
+			return $this->rawParams['size'];
+		}
+
+
+		public function getMimeType()
+		{
+			if ( class_exists('\FInfo') ) 
+			{
+				$info = \FInfo( FILEINFO_MIME_TYPE );
+				return $info->file( $this->getPath() );
+			}
+
+			return $this->rawParams['type'];
+		}
+		
+
+		public function getPath()
+		{
+			return $this->rawParams['tmp_name'];
+		}
+
+	}
