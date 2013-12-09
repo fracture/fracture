5c37ff0cd25c7bb28293850b46f5b67edfafe016
Mārtiņš Tereško <martins.teresko@live.com>
2013-12-10 00:26:35 +0200

    checks to verify validity for UploadedFile

--- src/lib/fracture/http/uploadedfile.php
+++ src/lib/fracture/http/uploadedfile.php
@@ -1,46 +1,69 @@
 <?php
-	
-	namespace Fracture\Http;
+    
+    namespace Fracture\Http;
 
-	class UploadedFile
-	{
+    class UploadedFile
+    {
 
-		private $rawParams = [];
+        private $rawParams = [];
 
-
-		public function __construct( $params )
-		{
-			$this->rawParams = $params;
-		}
+        private $type = null;
 
 
-		public function getName()
-		{
-			return $this->rawParams['name'];
-		}
+        public function __construct( $params )
+        {
+            $this->rawParams = $params;
+        }
 
 
-		public function getSize()
-		{
-			return $this->rawParams['size'];
-		}
+        public function prepare()
+        {
+            if ( class_exists('\FInfo') ) 
+            {
+                $info = new \FInfo( FILEINFO_MIME_TYPE );
+                $this->type = $info->file( $this->getPath() );
+            }
+        }
+        
+
+        public function isValid()
+        {
+            return  $this->rawParams['error'] === 0;
+        }
 
 
-		public function getMimeType()
-		{
-			if ( class_exists('\FInfo') ) 
-			{
-				$info = new \FInfo( FILEINFO_MIME_TYPE );
-				return $info->file( $this->getPath() );
-			}
-
-			return $this->rawParams['type'];
-		}
+        public function hasProperExtension()
+        {
+            return $this->rawParams['type'] === $this->type;
+        }
 
 
-		public function getPath()
-		{
-			return $this->rawParams['tmp_name'];
-		}
+        public function getName()
+        {
+            return $this->rawParams['name'];
+        }
 
-	}
+
+        public function getSize()
+        {
+            return $this->rawParams['size'];
+        }
+
+
+        public function getMimeType()
+        {
+            if ( $this->type !== null )
+            {
+                return $this->type;
+            }
+
+            return $this->rawParams['type'];
+        }
+
+
+        public function getPath()
+        {
+            return $this->rawParams['tmp_name'];
+        }
+
+    }
