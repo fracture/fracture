93543eae09d1b20679550d092b9589c299935d40
Mārtiņš Tereško <martins.teresko@live.com>
2013-12-17 00:48:30 +0200

    implementing FileBagBuilder class

--- src/lib/fracture/http/filebagbuilder.php
+++ src/lib/fracture/http/filebagbuilder.php
@@ -5,4 +5,62 @@
     class FileBagBuilder
     {
 
+        private $uploadedFileBuilder = null;
+
+
+        public function __construct( $builder )
+        {
+            $this->uploadedFileBuilder = $builder;
+        }
+
+
+        public function create( $list )
+        {
+            $instance = new FileBag;
+
+            foreach ( $list as $key => $value )
+            {
+                $item = $this->createItem( $value );
+                $instance->addItem( $key, $item );
+            }
+
+            return $instance;
+        }
+
+        private function createItem( $params )
+        {
+            if ( array_key_exists( 'name', $params ) === false  )
+            {
+                return null
+            }
+
+            if ( is_array( $list[ 'name' ] ) === true )
+            {
+                return $this->createFromLists( $params );
+            }
+
+            return $this->uploadedFileBuilder->create( $params );
+        }
+
+
+        private function createFromList( $list )
+        {
+            $instance = new FileBag;
+
+            foreach ( array_keys( $list['name'] ) as $key )
+            {
+                $params = [
+                    'name'      => $list['name'][$key],
+                    'type'      => $list['type'][$key],
+                    'tmp_name'  => $list['tmp_name'][$key],
+                    'error'     => $list['error'][$key],
+                    'size'      => $list['size'][$key],
+                ];
+                $file = $this->uploadedFileBuilder->create( $params );
+                $instance->addItem( $key, $file );
+            }
+
+            return $instance;
+        }
+
     }
---
+++ tests/unit/lib/fracture/http/filebagbuilderTest.php
@@ -0,0 +1,19 @@
+<?php
+
+
+    namespace Fracture\Http;
+
+    use Exception;
+    use ReflectionClass;
+    use PHPUnit_Framework_TestCase;
+
+
+    class FileBagBuilderTest extends PHPUnit_Framework_TestCase
+    {
+
+        public function test_nothing()
+        {
+
+        }
+
+    }
