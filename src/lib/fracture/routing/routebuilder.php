e3804f128905eaad32057a483b77538b8dc568ff
Mārtiņš Tereško <martins.teresko@live.com>
2013-12-17 22:48:47 +0200

    default values should be defined as such, not just magically assigned

--- src/lib/fracture/routing/routebuilder.php
+++ src/lib/fracture/routing/routebuilder.php
@@ -5,13 +5,15 @@
     class RouteBuilder
     {
 
+        private $defaults = [
+            'conditions' => [],
+            'defaults'   => [],
+            'notation'   => '',
+        ];
+
         public function create( $name, $parameters )
         {
-            $parameters += [
-                'conditions' => [],
-                'defaults'   => [],
-                'notation'   => '',
-            ];
+            $parameters += $this->defaults;
 
             $pattern = new Pattern( $parameters[ 'notation' ], $parameters[ 'conditions' ] );
             $instance = new Route( $pattern, $name, $parameters[ 'defaults' ] );
