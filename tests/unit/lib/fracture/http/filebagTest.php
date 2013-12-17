8e94a262fe8bf29c0980c56cb4b4810f7e0332d4
Mārtiņš Tereško <martins.teresko@live.com>
2013-12-17 23:34:55 +0200

    tests for iterator interface in FileBag class

--- tests/unit/lib/fracture/http/filebagTest.php
+++ tests/unit/lib/fracture/http/filebagTest.php
@@ -39,6 +39,25 @@
         /**
          * @covers Fracture\Http\FileBag::offsetExists
          * @covers Fracture\Http\FileBag::offsetSet
+         */
+        public function test_ArrayAccess_for_One_Valid_Item_without_Key()
+        {
+            $instance = new FileBag;
+
+
+            $item  = $this->getMock( 'Fracture\Http\UploadedFile', ['isValid'], [ 'foo' => 'bar'] );
+            $item->expects( $this->once() )
+                     ->method( 'isValid' )
+                     ->will( $this->returnValue( true ) );
+
+            $this->assertFalse( isset( $instance[ 0 ] ) );
+            $instance[] = $item;
+            $this->assertTrue( isset( $instance[ 0 ] ) );
+        }
+
+        /**
+         * @covers Fracture\Http\FileBag::offsetExists
+         * @covers Fracture\Http\FileBag::offsetSet
          * @covers Fracture\Http\FileBag::offsetGet
          * @covers Fracture\Http\FileBag::offsetUnset
          */
@@ -49,8 +68,8 @@
 
             $item  = $this->getMock( 'Fracture\Http\UploadedFile', ['isValid'], [ 'foo' => 'bar' ] );
             $item->expects( $this->once() )
-                     ->method( 'isValid' )
-                     ->will( $this->returnValue( false ) );
+                 ->method( 'isValid' )
+                 ->will( $this->returnValue( false ) );
 
             $this->assertFalse( isset( $instance[ 'foo' ] ) );
             $instance[ 'foo' ] = $item;
@@ -58,6 +77,95 @@
             $this->assertNull( $instance[ 'foo' ] );
             unset( $instance[ 'foo' ] );
             $this->assertFalse( isset( $instance[ 'foo' ] ) );
+        }
+
+
+        /**
+         * @covers Fracture\Http\FileBag::current
+         * @covers Fracture\Http\FileBag::key
+         * @covers Fracture\Http\FileBag::next
+         * @covers Fracture\Http\FileBag::rewind
+         * @covers Fracture\Http\FileBag::valid
+         */
+        public function test_Iterator_with_Foreach_Loop()
+        {
+
+            $first  = $this->getMock( 'Fracture\Http\UploadedFile', ['isValid'], [ 'foo' => 'bar' ] );
+            $first->expects( $this->once() )
+                     ->method( 'isValid' )
+                     ->will( $this->returnValue( true ) );
+
+            $second  = $this->getMock( 'Fracture\Http\UploadedFile', ['isValid'], [ 'foo' => 'bar' ] );
+            $second->expects( $this->once() )
+                     ->method( 'isValid' )
+                     ->will( $this->returnValue( true ) );
+
+            $third  = $this->getMock( 'Fracture\Http\UploadedFile', ['isValid'], [ 'foo' => 'bar' ] );
+            $third->expects( $this->once() )
+                     ->method( 'isValid' )
+                     ->will( $this->returnValue( true ) );
+
+            $instance = new FileBag;
+
+            $instance['a'] = $first;
+            $instance['b'] = $second;
+            $instance['c'] = $third;
+
+
+            $expected = [ 'a' =>  $first, 'b' => $second, 'c' => $third ];
+            $keys = ['a', 'b', 'c'];
+
+            foreach ( $instance as $key => $value )
+            {
+                $this->assertEquals( array_shift( $keys ), $key );
+                $this->assertEquals( $expected[$key], $value );
+            }
+        }
+
+
+        /**
+         * @covers Fracture\Http\FileBag::current
+         * @covers Fracture\Http\FileBag::key
+         * @covers Fracture\Http\FileBag::next
+         * @covers Fracture\Http\FileBag::rewind
+         * @covers Fracture\Http\FileBag::valid
+         */
+        public function test_Iterator_Directly_with_Numeric_Keys()
+        {
+
+            $first  = $this->getMock( 'Fracture\Http\UploadedFile', ['isValid'], [ 'foo' => 'bar' ] );
+            $first->expects( $this->once() )
+                     ->method( 'isValid' )
+                     ->will( $this->returnValue( true ) );
+
+            $second  = $this->getMock( 'Fracture\Http\UploadedFile', ['isValid'], [ 'foo' => 'bar' ] );
+            $second->expects( $this->once() )
+                     ->method( 'isValid' )
+                     ->will( $this->returnValue( true ) );
+
+            $third  = $this->getMock( 'Fracture\Http\UploadedFile', ['isValid'], [ 'foo' => 'bar' ] );
+            $third->expects( $this->once() )
+                     ->method( 'isValid' )
+                     ->will( $this->returnValue( true ) );
+
+            $instance = new FileBag;
+
+            $instance[] = $first;
+            $instance[] = $second;
+            $instance[] = $third;
+
+            $this->assertEquals( $first, $instance->current() );
+            $instance->next();
+            $this->assertEquals( 1, $instance->key() );
+            $instance->next();
+            $instance->next();
+            $this->assertFalse( $instance->valid() );
+            $this->assertEquals( 3, $instance->key() );
+            $instance->rewind();
+            $this->assertEquals( 0, $instance->key() );
+            $instance->next();
+            $this->assertEquals( $second, $instance->current() );
+
         }
 
     }
